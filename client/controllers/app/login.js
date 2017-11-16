affe275dfb68a978dcbf7209ac15115b195e442c
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-11-15 21:15:28 -0800

    [Studio] Modified screen "Login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,21 +1,28 @@
 angular.module('app').controller('app_login', app_login);
-function app_login($scope, app) {
+function app_login($scope, app, $q, powwowLoginNew) {
     'use strict';
     app.init($scope);
+ 
     $scope.login = function () {
-        $scope.doLogin({
-            username: $scope.data.username,
-            password: $scope.data.password
-        }, false);
-    };
-    $scope.doLogin = function () {
         $scope.app.showLoading('Logging in');
-        app.call('login.loginBasic',$scope.data);
-       
+        app.action('login', 'submit', this);
     };
-    
-    $scope.doAppLogin = function (credentials) {
-
-        $scope.doLogin(credentials, true);
-    };
+ 
+    app.origEstablishConnection = app.establishConnection;
+    app.establishConnection = function (params) {
+        if (app.alreadyConnected) {
+            console.log("Calling powwowLoginNew getcachedcredentials");
+            var credentials = powwowLoginNew.getCachedCredentials();
+            if (!credentials.username) {
+                console.log("No cached credentials");
+                // if no user credentials we cannot perform App login - forvard user to loginScreen
+                powwowLoginNew.clearCachedCredentials();
+                window.location.reload();
+                return;
+            }
+            app.call('login.loginBasic', credentials);
+        } else {
+            app.origEstablishConnection(params);
+        }
+    }
 }
