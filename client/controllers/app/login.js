19aeca3acef38076943f5bdb9136b2e4bfd12395
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-11-28 01:07:26 -0800

    [Studio] Modified screen "Login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,4 +1,11 @@
 angular.module('app').controller('app_login', app_login);
+function app_login($scope, app,$ionicLoading) {
+    'use strict';
+    app.init($scope);
+  
+} 
+
+/*angular.module('app').controller('app_login', app_login);
 function app_login($scope, app, $q, powwowLoginNew) {
     'use strict';
     app.init($scope);
@@ -25,4 +32,4 @@ function app_login($scope, app, $q, powwowLoginNew) {
             app.origEstablishConnection(params);
         }
     }
-}
+}*/
