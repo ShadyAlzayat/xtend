6e2485a4901e70956f48b6f037cbeb1e9a3529a5
Venugopal Ramamurthy <venugopal.ramamurthy@flex.com>
2017-12-05 20:47:16 -0800

    [Studio] Modified screen "home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -2,4 +2,30 @@ angular.module('app').controller('app_home', app_home);
 function app_home($scope, app) {
     'use strict';
     app.init($scope);
+    $scope.scan = function () {
+        cordova.plugins.barcodeScanner.scan(
+            // success callback function
+            function (result) {
+                // wrapping in a timeout so the dialog doesn't free the app
+                setTimeout(function () {
+                    //$scope.data.format = result.format;
+                    $scope.data.batchnumber = result.text;
+                    $scope.$digest();
+                }, 0);
+            },
+
+            // error callback function
+            function (error) {
+
+            },
+
+            // options object
+            {
+                "preferFrontCamera": false,
+                "showFlipCameraButton": true,
+                "showTorchButton": true,
+                "orientation": "landscape"
+            }
+        );
+    };
 }
